300754cd4864be542afaef5a4047f035fc180f5c
Natinael Boda Borana <natinael@Natinael.localdomain>
2025-06-13 16:29:49 +0200

    Added content to copilot_shell_script

---
+++ copilot_shell_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "What is the name of assignment to check submission status?: " assignment_name
+# Loading the content of create_environment.sh file. This is important to navigate to the folder later created by the file.
+source ./create_environment.sh
+# This line should go to inside of the config.env file
+source ./"submission_reminder_$name"/config/config.env
+# Substituting the value of ASSIGNMENT variable with the value the user entered
+sed i 's/ASSIGNMENT="Shell Navigation"/ASSIGNMENT="$assignment_name"/'
+
