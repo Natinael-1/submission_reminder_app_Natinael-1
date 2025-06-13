01ea39d358e06bc85c76288b05605be95e9c5687
Natinael Boda Borana <natinael@Natinael.localdomain>
2025-06-13 16:56:36 +0200

    Updated the content of copilot_shell_script.sh

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -4,6 +4,10 @@ read -p "What is the name of assignment to check submission status?: " assignmen
 source ./create_environment.sh
 # This line should go to inside of the config.env file
 source ./"submission_reminder_$name"/config/config.env
+# Checking whether the directory for the config/config.env
+if [[ ! -d "submission_reminder_$name" ]]; then
+echo "The directory does not exist please run the create_environment.sh script first"
+fi
 # Substituting the value of ASSIGNMENT variable with the value the user entered
 sed i 's/ASSIGNMENT="Shell Navigation"/ASSIGNMENT="$assignment_name"/'
 
